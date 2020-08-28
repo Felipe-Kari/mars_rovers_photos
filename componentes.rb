10427736d39d808ed296b7fbec9b87b33bf27c38
Felipe Kari <felipe.kari@hotmail.com>
2020-08-28 13:11:54 -0400

    fix head & foot, body w imgs now

--- componentes.rb
+++ componentes.rb
@@ -4,8 +4,8 @@ require_relative 'request'
 # api_key = 'dJ8J0ttqoVilZVljNXysOYeb1pzwRsewyq3aqxfs'
 
 def request(url, api_key)
-    photos = get_data("#{url}" + "#{api_key}")
-    return photos
+    data = get_data("#{url}" + "#{api_key}")
+    return data
 end
 
 # api_roverph = request(api_end, api_key)
@@ -13,27 +13,34 @@ end
 #build_web_page
 
 def head
-    head = "
-    <html>
-    <head>
-    </head>
-    <body>
-    "
+    head = "<html>\n"+
+    "<head>\n"+
+    "</head>\n"+
+    "<body>\n"
 end
 
 def body
     api_end = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key='
     api_key = 'dJ8J0ttqoVilZVljNXysOYeb1pzwRsewyq3aqxfs'
-    
-    body = "#{request(api_end, api_key)}"
+
+    api = request(api_end, api_key)
+
+    body = "<ul>\n"
+    # body += "\n#{api["photos"]}"
+    api["photos"].each do |k1, v|
+        k1.each do |k2, v|
+            if k2 == "img_src"
+                body += "\t<li><img src='#{v}'></li>\n"
+            end
+        end
+    end
+    body += "</ul>\n"
 end
 
 
 def foot
-    foot = "
-    </body>
-    </html>
-    "
+    foot = "</body>\n"+
+            "</html>"
 end
 
 File.write('./index.html', (head() + body() + foot()))
