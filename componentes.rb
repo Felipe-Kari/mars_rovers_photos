8e9914e70794c9757c3835664a666860c7532f5f
Felipe Kari <felipe.kari@hotmail.com>
2020-08-28 11:42:54 -0400

    add met request

--- componentes.rb
+++ componentes.rb
@@ -1,1 +1,11 @@
 require_relative 'request'
+
+url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key='
+api_key = 'dJ8J0ttqoVilZVljNXysOYeb1pzwRsewyq3aqxfs'
+
+def request(url, api_key)
+    photos = get_data("#{url}" + "#{api_key}")
+    return photos
+end
+
+# puts request(url, api_key)
