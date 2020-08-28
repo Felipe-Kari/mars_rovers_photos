ce750401b2481507b67967abf7d87e7756491dae
Felipe Kari <felipe.kari@hotmail.com>
2020-08-28 14:31:49 -0400

    add met build_web_page

--- componentes.rb
+++ componentes.rb
@@ -1,14 +1,11 @@
 require_relative 'request'
 
-# api_end = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key='
-# api_key = 'dJ8J0ttqoVilZVljNXysOYeb1pzwRsewyq3aqxfs'
+#request
 
-def request(url, api_key)
+def requestb(url, api_key)
     data = get_data("#{url}" + "#{api_key}")
     return data
 end
-
-# api_roverph = request(api_end, api_key)
 
 #build_web_page
 
@@ -19,16 +16,11 @@ def head
     "<body>\n"
 end
 
-def body
-    api_end = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key='
-    api_key = 'dJ8J0ttqoVilZVljNXysOYeb1pzwRsewyq3aqxfs'
-
-    api = request(api_end, api_key)
-
+def body(api)
     body = "<ul>\n"
-    # body += "\n#{api["photos"]}"
-    api["photos"].each do |k1, v|
-        k1.each do |k2, v|
+    
+    api["photos"].each do |k|
+        k.each do |k2, v|
             if k2 == "img_src"
                 body += "\t<li><img src='#{v}'></li>\n"
             end
@@ -42,5 +34,3 @@ def foot
     foot = "</body>\n"+
             "</html>"
 end
-
-File.write('./index.html', (head() + body() + foot()))
--- nasa_page.rb
+++ nasa_page.rb
@@ -1,1 +1,12 @@
 require_relative 'componentes'
+
+def build_web_page
+    api_end = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key='
+    api_key = 'dJ8J0ttqoVilZVljNXysOYeb1pzwRsewyq3aqxfs'
+
+    index = head() + body(requestb(api_end, api_key)) + foot()
+
+    return index
+end
+
+File.write('./index.html', build_web_page())
